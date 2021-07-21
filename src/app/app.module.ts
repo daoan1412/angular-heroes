909e3d092d9eb4129e56596ec6f533acd4649818
Dao An <daoan1412@gmail.com>
2021-07-21 17:10:46 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HerosComponent,
+    AppComponent,
+    HeroDetailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
